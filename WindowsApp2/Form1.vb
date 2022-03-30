ab5692cd8c3ec1efb0a9b1738f8506f0d7542336
ertanay <ertanay8@gmail.com>
2022-03-30 15:22:40 +0300

    Vücut kitle endeksi hesaplama
    Visual Basic ile vücut kitle endeksi hesaplama ve timer ekleme.

---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim boy, kilo, sonuc As Single
+        boy = Val(TextBox1.Text)
+        kilo = Val(TextBox2.Text)
+        sonuc = kilo / (boy / 100) ^ 2
+        MsgBox("VKİ niz " & Math.Round(sonuc, 3))
+
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label3.Text = TimeOfDay
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
